ce9bcd1e2614fd209e949c1c672784d84fbb63b4
Sean Cleaver <sean@simego.com>
2022-08-04 10:38:39 +0100

    Initial Commit

---
+++ src/Simego.DataSync.Providers.PostgreSql/PostgreSqlDatasourceReader.cs
@@ -0,0 +1,28 @@
+using Simego.DataSync.Providers.Ado;
+using System;
+using System.Data.Common;
+using System.Globalization;
+using System.Reflection;
+
+namespace Simego.DataSync.Providers.PostgreSql
+{
+    [ProviderInfo(Name = "PostgreSql (6.0.5)", Description = "Npgsql for PostgreSql", Group = "SQL")]
+    public class PostgreSqlDatasourceReader : AdoDataSourceReader
+    {
+        private Lazy<AdoDbProviderFactory> MyFactory => new Lazy<AdoDbProviderFactory>(() => new AdoDbProviderFactory("Npgsql", GetFactory()));
+
+        protected override bool IsCustomAdoProvider() => true;
+
+        public override AdoDbProviderFactory GetProviderFactory(string providerInvariantName) => MyFactory.Value;
+
+        private static DbProviderFactory GetFactory()
+        {
+            var factoryType = typeof(Npgsql.NpgsqlFactory);
+
+            return factoryType.InvokeMember(factoryType.Name,
+               BindingFlags.CreateInstance | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+               null, null, null, CultureInfo.CurrentCulture) as DbProviderFactory;
+        }
+    }
+}
+
